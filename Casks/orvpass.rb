a1ddc58a37b598368256a8aec48af4cf1e21594e
Kartavya Singh <kartxvyaa@gmail.com>
2026-08-25 01:49:39 +0530

    fix: add exact sha256 checksum for v4.1.2

--- Casks/orvpass.rb
+++ Casks/orvpass.rb
@@ -1,6 +1,6 @@
 cask "orvpass" do
   version "4.1.2"
-  sha256 :no_check
+  sha256 "252260fe4fb98ad58bb52fcfa06cc892456c7913c208c38ff8cc487947aee6a2"
 
   url "https://github.com/krtvysinghh/Orvpass/releases/download/v#{version}/Orvpass_#{version}_aarch64.dmg"
   name "Orvpass"
