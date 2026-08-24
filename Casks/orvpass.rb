8fa5fa520f4106105681eb5392a589b8907f8efe
Kartavya Singh <kartxvyaa@gmail.com>
2026-08-24 19:43:43 +0530

    fix: update SHA256 for 4.0.8

--- Casks/orvpass.rb
+++ Casks/orvpass.rb
@@ -1,6 +1,6 @@
 cask "orvpass" do
   version "4.0.8"
-  sha256 :no_check
+  sha256 "97b41c54b82cc209ae37b84f2e54f977b704940520b63de3e6b6720844e45c7a"
 
   url "https://github.com/krtvysinghh/Orvpass/releases/download/v#{version}/Orvpass_#{version}_aarch64.dmg"
   name "Orvpass"
